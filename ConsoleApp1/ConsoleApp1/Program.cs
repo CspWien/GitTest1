c8676953103429e1cd67b21b12c9a8b40eaa4c01
LiBing <libinghome@outlook.com>
2018-08-27 15:30:51 +0200

    Edited code

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -9,6 +9,7 @@ namespace ConsoleApp1
             Console.WriteLine("Hello World!");
             
             // Code was edited in GitHub
+            // Code was added in VS
         }
     }
 }
