8158f17ffe4f4bc4a5862bb19528e7c49b1ac73b
CspWien <42736370+CspWien@users.noreply.github.com>
2018-08-27 15:28:56 +0200

    Update Program.cs

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -8,7 +8,7 @@ namespace ConsoleApp1
         {
             Console.WriteLine("Hello World!");
             
-            // Code was added in GitHub
+            // Code was edited in GitHub
         }
     }
 }
